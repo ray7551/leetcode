518cc918acf18e4e10666948c082104290e97440
ray7551 <ray7551@gmail.com>
2016-04-22 00:14:16 +0800

    add question

--- algorithms/28. Implement strStr()/strStr.js
+++ algorithms/28. Implement strStr()/strStr.js
@@ -1,4 +1,10 @@
 /**
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+
+/**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
